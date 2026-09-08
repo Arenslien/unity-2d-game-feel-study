78d7c818145a78575226d670ab7816d56fc19f47
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-08 17:19:17 +0900

    feat: 마우스 포인터 기능 구현

---
+++ Assets/02.Scripts/MousePointer.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+public class MousePointer : MonoBehaviour
+{
+    // 성능 최적화를 위해 메인 카메라 캐싱
+    private Camera _mainCamera;
+    
+    private void Start()
+    {
+        _mainCamera = Camera.main;
+    }
+
+    private void Update()
+    {
+        // 1. 유리창에 짚은 손가락 위치
+        Vector3 mouseScreenPosition = Input.mousePosition;
+        Debug.Log(mouseScreenPosition);
+
+        // 2. 유리창에서 실제 게임 오브젝트가 있는 위치까지의 깊이(z) 보정
+        mouseScreenPosition.z = Mathf.Abs(_mainCamera.transform.position.z);
+        
+        // 3. 월드 좌표로의 변환
+        Vector3 mouseWorldPosition = _mainCamera.ScreenToWorldPoint(mouseScreenPosition);
+        
+        // 4. 실제 게임 오브젝트들이 있는 위치의 Z로 고정
+        mouseWorldPosition.z = 0f;
+        
+        transform.position = mouseWorldPosition;
+    }
+}
