1187b25d8dcab785a18e6cd53d61205bd345ae50
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-10 18:22:42 +0900

    chore: 집 가서 작업 더 해야지

--- Assets/02.Scripts/Item/Torch.cs
+++ Assets/02.Scripts/Item/Torch.cs
@@ -9,6 +9,9 @@ public class Torch : Item
 	[SerializeField] private float _flickerSpeed = 1f;
 	[SerializeField] private float _flickerAmount = 0.5f;
 
+	private float BaseIntensity = 1.0f;
+	
+	
     private void Start()
     {
 		_light2D = GetComponent<Light2D>();
@@ -17,6 +20,9 @@ public class Torch : Item
 	private void Update()
 	{
 		if (_light2D == null) return;
+		
+		// 현재 시간에 속도를 곱하고 무작위 시작점을 더한다.
+		// float currentTime = (Time.time * );
 
 		// Mathf.PerlinNoise를 사용하여 불규칙하고 자연스러운 난수(0~1)를 생성
 		float noise = Mathf.PerlinNoise(Time.time * _flickerSpeed, 0f);
