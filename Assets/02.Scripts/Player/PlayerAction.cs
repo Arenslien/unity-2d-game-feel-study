34ad4b4f7ebd4af3522a19ae2951629f671028e0
Sunghoon Jung <46517668+Arenslien@users.noreply.github.com>
2026-09-10 18:01:42 +0900

    feat: 아이템 버리기 기능 구현
    - ThrowItem의 코드에서 _throwForce를 const float _dumpForce로 변경

--- Assets/02.Scripts/Player/PlayerAction.cs
+++ Assets/02.Scripts/Player/PlayerAction.cs
@@ -6,7 +6,8 @@ public class PlayerAction : MonoBehaviour
     private MousePointer _mousePointer;
 
     [SerializeField] private float _throwForce = 1f;
-
+    private const float _dumpForce = 3f;
+    
     private void Awake()
     {
         _inventory = GetComponent<Inventory>();
@@ -18,6 +19,8 @@ public class PlayerAction : MonoBehaviour
         // 1. 아이템 들고 있는지 체크 후 아이템 던지기
         bool isSelected = _inventory.IsSelected;
         if (Input.GetMouseButtonDown(1) && isSelected) ThrowItem();
+        
+        if (Input.GetKeyDown(KeyCode.T) && isSelected) DumpItem();
     }
 
     private void ThrowItem()
@@ -40,6 +43,30 @@ public class PlayerAction : MonoBehaviour
         if (rb != null)
         {
             rb.AddForce(direction * _throwForce, ForceMode2D.Impulse);
+        }
+    }
+
+    private void DumpItem()
+    {
+        Debug.Log($"버리자");
+        
+        // 1. 현재 들고 있는 횃불 비활성화
+        _inventory.EquipItem();
+        
+        // 2. 횃불 초기 위치 & 마우스 포인트 위치 계산
+        Vector3 torchPosition = _inventory.EquippedTorch.transform.position;
+        Vector3 mouseWorldPosition = _mousePointer.GetMouseWorldPosition();
+        
+        // 3. 방향 구하기
+        Vector3 direction = (mouseWorldPosition - torchPosition).normalized;
+        
+        // 4. 횃불 생성 후 던지기
+        GameObject torch = Instantiate(_inventory.ThrowableTorch, torchPosition, Quaternion.identity);
+
+        Rigidbody2D rb = torch.GetComponent<Rigidbody2D>();
+        if (rb != null)
+        {
+            rb.AddForce(direction * _dumpForce, ForceMode2D.Impulse);
         }
     }
 }
